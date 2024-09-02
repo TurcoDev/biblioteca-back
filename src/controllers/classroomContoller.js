6c7c95f3cabe1ddb9674480970f32dbb892be19a
benjikpop202 <benjaminsobarzo2018@gmail.com>
2024-09-01 21:44:38 -0300

    agregando funcinalidad a las aulas

--- src/controllers/classroomContoller.js
+++ src/controllers/classroomContoller.js
@@ -23,7 +23,50 @@ const getAulas = async (req, res) => {
   
   };
 
+  const AddAula = async (req, res)=>{
+    const Data = req.body
+    try {
+      const NewAula = Classroom.create(Data)
+      res.status(201).send("Aula Creada")
+      console.log(NewAula);
+    } catch (error) {
+      res.status(500).send(error)
+    }
+  }
+
+  const UpdateAula = async (req, res)=>{
+    const id = req.params.id
+    const name = req.body
+    try {
+      const aula = await Classroom.findByPk(id);
+      if(!aula){
+        res.status(404).send("Aula no encontrada")
+      }else{
+        Classroom.name = name
+        await Classroom.save()
+      }
+    } catch (error) {
+      res.status(500).send(error)
+    }
+  }
+
+  const DeleteAula = async (req, res)=>{
+    const id = req.params.id
+    try {
+      const Delete = await Classroom.destroy({ where: { classroom_library_id: id } });
+      if (Delete === 0) {
+        return res.status(404).send("Aula no encontrada");
+      }
+      res.status(200).send("Aula eliminada correctamente");
+    } catch (error) {
+      res.status(500).send(error)
+    }
+  }
+
   module.exports = {
     getAulaById,
-    getAulas
+    getAulas,
+    AddAula,
+    UpdateAula,
+    DeleteAula
   }
