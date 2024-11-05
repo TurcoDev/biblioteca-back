49b7a25a9bc20f1504370bfd4972bbd6d506a78b
benjikpop202 <benjaminsobarzo2018@gmail.com>
2024-11-05 20:15:38 -0300

    editando ruta put de libro

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -6,7 +6,7 @@ const bookController = require('../controllers/bookController.js');
 router.get('/', bookController.getAllBooks);
 router.get('/:id', bookController.getBookById);
 router.post('/',upload.single('portada'), bookController.createBook);
-router.put('/:id', bookController.updateBook);
+router.put('/:id',upload.single('portada'), bookController.updateBook);
 router.delete('/:id', bookController.deleteBook);
 
 module.exports = router;
